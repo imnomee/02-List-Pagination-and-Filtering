6c66dd6d2a9c352b233c82069477cb8cf0d64871
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-31 13:05:46 +0000

    Updated code and meets expectation

--- js/script.js
+++ js/script.js
@@ -5,39 +5,11 @@ FSJS project 2 - List Filter and Pagination
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const studentList = document.querySelectorAll('ul li');
-const resultsPerPage = 10;
-const totalStudents = studentList.length;
-const numOfPages = Math.ceil(totalStudents / resultsPerPage);
+const resultsPerPage = 10; //results per page, we can change it any time to display different results
+const totalStudents = studentList.length; // total students
+const numOfPages = Math.ceil(totalStudents / resultsPerPage); //total number of pages it will generate
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 function showPage(list, page) {
    let currentPage = (page * resultsPerPage);
    for (let i = 0; i < list.length; i++) {
@@ -49,56 +21,44 @@ function showPage(list, page) {
          list[i].style.display = 'none';
       }
    }
-
 }
 
-showPage(studentList, 6);
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
 
 function appendPageLinks(studentList) {
    const page = document.querySelector('.page');
    const paginationDiv = document.createElement('div');
    paginationDiv.className = 'pagination';
    page.appendChild(paginationDiv);
-
    const paginationUl = document.createElement('ul');
    paginationDiv.appendChild(paginationUl);
 
    for (let i = 1; i <= numOfPages; i++) {
       const list = document.createElement('li');
       const listLink = document.createElement('a');
-
       listLink.href = '#';
       listLink.textContent = i;
       list.appendChild(listLink);
       paginationUl.appendChild(list);
+
       listLink.addEventListener('click', (e) => {
          const page = e.target.textContent;
          // console.log('clicked the button ' + listLink.textContent);
-         // listLink.className = 'active';
-         for (let i = 0; i < numOfPages; i++) {
-            showPage(studentList, page);
-            e.target.className = 'active';
+         const allActive = document.getElementsByClassName('active');
+         for (let i = 0; i < allActive.length; i++) {
+            allActive[i].className = '';
          }
-
-
+         showPage(studentList, page);
+         e.target.className = 'active';
       });
-
    }
+
+   //Getting all the generated List > a items and setting first link class to 'active'
+   const listItems = paginationUl.querySelectorAll('li a')[0];
+   listItems.className = 'active';
 }
+
+//Running showpage function with page number one by default. So it starts always with page 1
+showPage(studentList, 1);
+
 appendPageLinks(studentList);
 
-
-
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
